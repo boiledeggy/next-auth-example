32479b2f231a03011b1369edf31b3971a930e8c2
boiledeggy <misaki.kimoto.de@gmail.com>
2025-05-08 11:04:09 +0900

    Update route.ts

--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -1,9 +1,36 @@
-import { auth } from "auth"
+import { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
-export const GET = auth((req) => {
-  if (req.auth) {
-    return Response.json({ data: "Protected data" })
-  }
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Ninjin Sirisiri',
+      credentials: {
+        id: {
+          label: 'Id',
+          type: 'text',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // credentials に入力が渡ってくる
+        // id, password はここでベタ打ちして検証している
+        const matched =
+          credentials?.id === 'id' && 
+          credentials?.password === 'password'
+        if (matched) {
+          // 今回は null を返さなければなんでもよいので適当
+          return {
+            id: '29472084752894723890248902',
+          }
+        } else {
+          return null
+        }
+      },
+    }),
+  ],
+}
 
-  return Response.json({ message: "Not authenticated" }, { status: 401 })
-})
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
